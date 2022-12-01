f1589a124a9a1afbbb8fc5cc2856ed594ac9dcc8
isipringlejr <modesttechnologiessolutions@gmail.com>
2022-12-01 15:38:57 +0000

    conflict2 resolved

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 # this is a deployment script
 deploy java application 
 deploy python apps 
 deploy in the dev env 
-=======
-# this is a deployment script 
 deployment4
->>>>>>> dev
+deployment5 master 
+deploy to staging environment 
+echo "good job"
+Tested and accepted 
